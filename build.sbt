c730983b6397a7a55deed31f873a2e4d72995118
Yoshihiro MIYAI <msparrow17@gmail.com>
2015-12-21 04:34:20 +0900

    Add SparkApp

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "spark-ml-adxl345-from-kinesis"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
---
+++ src/main/scala/App.scala
@@ -0,0 +1,1 @@
+case class App(sepalLength: Float, sepalWidth: Float, petalLength: Float, petalWidth: Float, species: String)
